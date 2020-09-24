96f29add5eac0074d94f9590c7b5422ba5723f90
RRCoder <you@example.com>
2020-09-24 00:25:15 -0300

    com 2

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_mysqldb import MySQL
 app = Flask(__name__, static_folder="static")
 
-app.config['MYSQL_HOST']    = 'bbl.mysql.pythonanywhere-services.com'
-app.config['MYSQL_USER']    = 'bbl'
-app.config['MYSQL_PASSWORD']= 'marzer1279bbl'
+
+app.config['MYSQL_HOST']    = 'localhost'
+app.config['MYSQL_USER']    = 'root'
+app.config['MYSQL_PASSWORD']= 'flash'
 app.config['MYSQL_DB']      = 'bbl$chery'
 
 mysql = MySQL(app)
